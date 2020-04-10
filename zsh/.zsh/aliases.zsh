d3a9a5b3f7fc2a3ec32bbec0237aa510d8a947fe
Jeffrey Cordova <jeffcordova@protonmail.com>
2020-04-10 07:49:51 -0700

    add witcher wallpaper

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -6,9 +6,10 @@
 #                                  
 
 #---[colors]--------------------------------------------------------------------
-alias ls="ls_extended"
-alias l="ls -lh"
-alias la="ls -alh"
+alias ls="exa --icons"
+#alias ls="ls_extended"
+alias l="ls -lhg"
+alias la="ls -lahg"
 
 alias grep="grep --color=auto"
 
