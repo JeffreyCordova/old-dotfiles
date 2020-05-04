406fb32fff4919254563206f98bcc602a3816579
Jeffrey Cordova <jeffcordova@protonmail.com>
2020-05-04 10:06:13 -0700

    added julia

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,6 +1,7 @@
 atreplinit() do rel
     try
         @eval using OhMyREPL
+        @eval colorscheme!("Base16MaterialDarker")
     catch e
         @warn "error while importing OhMyREPL" e
     end
