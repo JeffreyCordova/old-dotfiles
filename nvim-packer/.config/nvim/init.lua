676d91e0a5b8a5b05eba8eaed14560ef7305e76c
Jeffrey Cordova <jeffcordova@protonmail.com>
2022-10-28 12:03:09 -0700

    packer developments

--- nvim-packer/.config/nvim/init.lua
+++ nvim-packer/.config/nvim/init.lua
@@ -1,27 +1,3 @@
-local c = require('vscode.colors')
-require('vscode').setup({
-  -- Enable transparent background
-  transparent = true,
-
-  -- Enable italic comment
-  italic_comments = true,
-
-  -- Disable nvim-tree background color
-  disable_nvimtree_bg = true,
-
-  -- Override colors (see ./lua/vscode/colors.lua
-  color_overrides = {
-    vscLineNumber = '#FFFFFF',
-  },
-
-  -- Override highlight groups (see ./lua/vscode/theme.lua)
-  group_overrides = {
-    -- this supports the same val table as vim.api.nvim_set_hl
-    -- use colors from this colorscheme by requiring vscode.colors!
-    Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true }
-  }
-})
-
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -36,10 +12,9 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+  use "wbthomason/packer.nvim"
+
+  use "olimorris/onedarkpro.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
@@ -47,3 +22,5 @@ return require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
+
+vim.cmd [[colorscheme onedarkpro]]
