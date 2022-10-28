644f68a98ab9ab737071cb0b0cbb18eaa259a664
Jeffrey Cordova <jeffcordova@protonmail.com>
2022-10-28 08:32:22 -0700

    add packer

--- nvim-packer/.config/nvim/init.lua
+++ nvim-packer/.config/nvim/init.lua
@@ -1,23 +1,49 @@
 local c = require('vscode.colors')
 require('vscode').setup({
-    -- Enable transparent background
-    transparent = true,
+  -- Enable transparent background
+  transparent = true,
 
-    -- Enable italic comment
-    italic_comments = true,
+  -- Enable italic comment
+  italic_comments = true,
 
-    -- Disable nvim-tree background color
-    disable_nvimtree_bg = true,
+  -- Disable nvim-tree background color
+  disable_nvimtree_bg = true,
 
-    -- Override colors (see ./lua/vscode/colors.lua
-    color_overrides = {
-        vscLineNumber = '#FFFFFF',
-    },
+  -- Override colors (see ./lua/vscode/colors.lua
+  color_overrides = {
+    vscLineNumber = '#FFFFFF',
+  },
 
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
-    group_overrides = {
-        -- this supports the same val table as vim.api.nvim_set_hl
-	-- use colors from this colorscheme by requiring vscode.colors!
-	Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true }
-    }
+  -- Override highlight groups (see ./lua/vscode/theme.lua)
+  group_overrides = {
+    -- this supports the same val table as vim.api.nvim_set_hl
+    -- use colors from this colorscheme by requiring vscode.colors!
+    Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true }
+  }
 })
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
