dc2fa795ddc9e113fca70ddb69b6f5ca181f676e
Jeffrey Cordova <jeffcordova@protonmail.com>
2022-11-03 19:51:13 -0700

    add wb config folders

---
+++ zsh-wb/.zsh/load_plugins.zsh
@@ -0,0 +1,14 @@
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-saneopt/saneopt.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-willghatch-SLASH-zsh-saneopt )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-changyuheng-SLASH-zsh-interactive-cd )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/wil/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
